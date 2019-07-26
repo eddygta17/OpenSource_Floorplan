9e8d01a4824ac23dee8c5bc077141c0d5648227f
abel <eddy.gta17@gmail.com>
2019-07-26 11:00:21 +0530

    Created script for permission to execute files

---
+++ permissions.sh
@@ -0,0 +1,7 @@
+chmod 777 read_lef.tcl
+chmod 777 read_lib.tcl
+chmod 777 read_verilog.tcl
+chmod 777 create_my_core.tcl
+chmod 777 create_my_die.tcl
+chmod 777 magic_create.sh
+chmod 777 magic_open.sh
