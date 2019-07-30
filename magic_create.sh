1d3b99e888bd6b96974f388ec5115336e5a3b80f
abel <eddy.gta17@gmail.com>
2019-07-30 10:36:57 +0530

    Updated magic_create to use tech file

--- magic_create.sh
+++ magic_create.sh
@@ -1,10 +1,10 @@
-magic -dnull -noconsole << EOF
+magic -T /usr/local/share/qflow/tech/osu018/SCN6M_SUBM.10.tech -dnull -noconsole << EOF
 drc off
 lef read lef/osu018_stdcells.lef
 lef read lef/picorv32.lef
 lef read lef/picosoc_mem.lef
 def read $1
 select top cell
-writeall force ${cellname}
+save ${cellname}
 quit -noprompt
 EOF
